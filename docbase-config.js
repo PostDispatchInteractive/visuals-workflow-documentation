9c7c5a1e4e1a2b93a8511069e13e198cef40543a
Carl V. Lewis <carl@carlvlewis.net>
2016-04-17 12:34:56 -0500

    Replacing files

---
+++ docbase-config.js
@@ -0,0 +1,76 @@
+var docbaseConfig = {
+  "method": "github",
+  "generic": {
+    "baseurl": "",
+    "path": ""
+  },
+  "file": {
+    "path": ""
+  },
+  "github": {
+    "user": "PostDispatchInteractive",
+    "repo": "visuals-workflow-documentation",
+    "path": "",
+    "branch": "master",
+    "access_token": ""
+  },
+  "indexHtml": "./html/main.html",
+  "flatdocHtml": "./bower_components/docbase/html/flatdoc.html",
+  "default_version": "",
+  "manual_override": false,
+  "versions" : {
+  "v1.0": [
+    {
+      "label": "Folder 1",
+      "name": "folder1",
+      "files": [
+        {
+          "label": "File 1",
+          "name": "file1"
+        }
+      ]
+    },
+    {
+      "label": "Folder 2",
+      "name": "folder2",
+      "files": [
+        {
+          "label": "File 1",
+          "name": "file1"
+        },
+        {
+          "label": "File 2",
+          "name": "file2"
+        }
+      ]
+    }
+  ],
+  "v2.0": [
+    {
+      "label": "Folder 1",
+      "name": "folder1",
+      "files": [
+        {
+          "label": "File 1",
+          "name": "file1"
+        }
+      ]
+    },
+    {
+      "label": "Folder 2",
+      "name": "folder2",
+      "files": [
+        {
+          "label": "File 1",
+          "name": "file1"
+        },
+        {
+          "label": "File 2",
+          "name": "file2"
+        }
+      ]
+    }
+  ]
+},
+  "publish": "github"
+}
